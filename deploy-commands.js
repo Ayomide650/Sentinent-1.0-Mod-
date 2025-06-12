28371dcd115c842b61c5978fed67286f8d6e4482
Ayomide650 <ahmedayomide1002@gmail.com>
2025-06-12 23:03:41 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,4 +4,37 @@
 // - Handles command cleanup
 // - Development vs production deployment
 
-// ...implementation goes here...
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const commands = [];
+const commandFolders = fs.readdirSync('./src/commands');
+
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./src/commands/${folder}/${file}`);
+    if (command.data) {
+      commands.push(command.data.toJSON());
+    }
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    // Register globally (can take up to 1 hour to update)
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
